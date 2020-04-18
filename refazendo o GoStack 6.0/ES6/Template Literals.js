aedbf37e8befaf590547c787f3b3bf67e6ee99c4
Raylan Sales <Raylanwork@gmail.com>
2020-04-18 16:47:51 -0300

    Create Template Literals.js

---
+++ refazendo o GoStack 6.0/ES6/Template Literals.js
@@ -0,0 +1,6 @@
+//Template Literals
+const nome = 'Rayxan';
+const idade = 19;
+
+//console.log('Meu nome é ' + nome + ' e minha idade é ' + idade);
+console.log(`Meu nome é ${nome} e minha idade é ${idade}`);
