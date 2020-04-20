7f28404d2d14fd73f88d833640f5b7832bb54077
Raylan Sales <Raylanwork@gmail.com>
2020-04-20 00:58:11 -0300

    Create iterator next e prev.cpp

---
+++ códigos úteis/iterator next e prev.cpp
@@ -0,0 +1,7 @@
+
+//Conceito de Iterator
+/*Iterators are used to point at the memory addresses
+of STL containers. They are primarily used in sequence
+of numbers, characters etc. They reduce the complexity
+and execution time of program.
+*/
