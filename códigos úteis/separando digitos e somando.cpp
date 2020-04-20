0d9fbdb21e9ba5203ef5120ebee5cedb09c2aad5
Raylan Sales <Raylanwork@gmail.com>
2020-04-20 00:04:19 -0300

    Update separando digitos e somando.cpp

--- códigos úteis/separando digitos e somando.cpp
+++ códigos úteis/separando digitos e somando.cpp
@@ -1,35 +1,26 @@
-//Esse programa soma os digitos de um numero ex: (12) = 1 + 2 = 3
+//Somar dígitos do input e dividir pelo próprio input;
 #include<bits/stdc++.h>
 using namespace std;
 
-int digits_sum(int N)
-{
-	int sum = 0;
-	
-	while  (N > 0)
-	{
-		int d = N % 10;
-		N /= 10;
-		
-		sum += d;
-	}
-	
-	return sum;
+int soma_digitos(int n){
+    int sum,digit;
+    while(n){
+        digit = n%10;
+        n/=10;
+
+        sum += digit;
+    }
+    return sum;
 }
 
 int main(){
     int n;
     cin>>n;
-    cout<<digits_sum(n);
+
+    if(n % soma_digitos(n) == 0)
+        cout<<"Yes";
+    else
+        cout<<"No";
+    
     return 0;
 }
-//explicação do professor: 
-/*
-você pode fazer um laço
-e tirar um dígito por vez
-com o operador %
-e atualizar o número dividindo-o por dez
-
-você vai extrair sempre as unidades do número que resta
-*/
-
