e9e3676f6a9b56d5e2f40b1a0ad1fd73a6020716
Raylan Sales <Raylanwork@gmail.com>
2020-05-08 13:07:39 -0300

    Create async-await.js

---
+++ refazendo o GoStack 6.0/ES6-mod-3/Desafio/async-await.js
@@ -0,0 +1,21 @@
+//Async await inviabiliza o uso de promises sem o .then e o .catch
+const minhaPromise = () => new Promise((resolve, reject) => {
+    setTimeout(() => { resolve('OK') }, 2000);
+});
+
+//Sem arrow funct
+async function executaPromise(){
+        console.log(await minhaPromise());
+        console.log(await minhaPromise());
+        console.log(await minhaPromise());
+}
+
+//Com arrow funct
+const executaPromise2 = async () => {
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
+};
+
+executaPromise();
+executaPromise2();
