20697de240cfba3cfb044fc867d06e420740e8cd
Raylan Sales <Raylanwork@gmail.com>
2020-05-08 13:28:19 -0300

    Update axios.js

--- refazendo o GoStack 6.0/ES6-mod-3/Desafio/axios.js
+++ refazendo o GoStack 6.0/ES6-mod-3/Desafio/axios.js
@@ -1,3 +1,18 @@
 /* axios é uma biblioteca que nos da acesso a uma API para podermos trabalhar com
 requisições assíncronas (protocolos http, servidores backend e etc)*/
 
+import axios from 'axios';
+
+class Api{
+    static async getUserInfo(username){
+        try{
+            const response = await axios.get(`https://api.github.com/users/${username}`);
+            console.log(response);
+        }catch(err){
+            console.warn("Erro na API");
+        }
+    }
+}
+
+Api.getUserInfo('rayxan');
+Api.getUserInfo('rayxan546644465465645654');
