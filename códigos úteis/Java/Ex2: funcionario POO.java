08a84b3d41f1aa8bbdacac8a73efa04ae8b0772e
Raylan Sales <Raylanwork@gmail.com>
2020-07-12 13:14:33 -0300

    Update Ex2: funcionario POO.java

--- códigos úteis/Java/Ex2: funcionario POO.java
+++ códigos úteis/Java/Ex2: funcionario POO.java
@@ -38,40 +38,32 @@ public class Program{
 
 //------------------------------------------------------------------------------------------------
 
-Funcionario.Java
+//Funcionario.java
 
-package exercise;
+package entities;
 
-import java.util.Scanner;
-import java.util.Locale;
-
-import entities.Funcionario;
-
-public class Program{
-	public static void main(String[] args) {
-		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
-		
-		Funcionario funcionario = new Funcionario();
-		
-		System.out.print("Name: ");
-		funcionario.name = sc.nextLine();
-		System.out.print("Gross Salary: ");
-		funcionario.grossSalary = sc.nextDouble();
-		System.out.print("Tax: ");
-		funcionario.tax = sc.nextDouble();
-		
-		System.out.println();
-		System.out.println(funcionario.print1());
-		
-		System.out.println();
-		System.out.print("Which percentage to increase salary? ");
-		double per = sc.nextDouble();
-		funcionario.increaseSalary(per);
-		
-		System.out.println();
-		System.out.println(funcionario.print2());
-		
-		sc.close();
+public class Funcionario{
+	public String name;
+	public double grossSalary;
+	public double tax;
+	
+	public double liqSalary() {
+		return grossSalary - tax;
+	}
+	
+	public void increaseSalary(double percent) {
+		grossSalary = (grossSalary-tax)+ (grossSalary * (percent/100));
+	}
+	
+	public String print1() {
+		return "Employee: "+
+				name + ", $"+
+				String.format("%.2f",liqSalary());
+	}
+	
+	public String print2() {
+		return "Updated data: "+ 
+				name +", $" +
+				String.format("%.2f",grossSalary);
 	}
 }
