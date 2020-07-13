1e7e61a17d492073b17ccc20f3e61e3c562210be
Raylan Sales <Raylanwork@gmail.com>
2020-07-12 22:14:58 -0300

    Update Função estática.java

--- códigos úteis/Java/Função estática.java
+++ códigos úteis/Java/Função estática.java
@@ -47,9 +47,6 @@ import java.util.Locale;
 import util.Calculator;
 
 public class Program{
-	
-	public final double PI = 3.14159; //variável estática
-	
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
@@ -76,12 +73,60 @@ public class Program{
 package util;
 
 public class Calculator {
-	public double PI = 3.14159;
+	public final double PI = 3.14159; //variável estática
 	
 	public double circunference(double radius) {
 		return 2.0 * PI * radius;
 	}
 	public double volume(double radius) {
+		return 4.0 * PI * radius * radius * radius / 3.0;
+	}
+}
+
+//-------------------------------------------------------------------------------
+//Mesme exemplo mas agora com métodos estáticos
+//obs: com métodos estáticos não é preciso instanciar a classe, basta usar o nome da classe na frente do método ou variável estática ao serem utilizados
+
+//Program.java
+
+package application;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+import util.Calculator;
+
+public class Program{
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter radius: ");
+		double radius = sc.nextDouble();
+		
+		double c = Calculator.circunference(radius);
+		
+		double v = Calculator.volume(radius);
+		
+		System.out.printf("Circunference: %.2f\n", c);
+		System.out.printf("Volume: %.2f\n", v);
+		System.out.printf("PI value: %.2f\n", Calculator.PI);
+		
+		sc.close();		
+	}
+}
+
+//Calculator.java
+
+package util;
+
+public class Calculator {
+	public static double PI = 3.14159;
+	
+	public static double circunference(double radius) {
+		return 2.0 * PI * radius;
+	}
+	public static double volume(double radius) {
 		return 4.0 * PI * radius * radius * radius / 3.0;
 	}
 }
