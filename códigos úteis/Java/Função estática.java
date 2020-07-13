eba5be6797c34ba135411a6727f80857159f0812
Raylan Sales <Raylanwork@gmail.com>
2020-07-12 22:03:32 -0300

    Update Função estática.java

--- códigos úteis/Java/Função estática.java
+++ códigos úteis/Java/Função estática.java
@@ -35,3 +35,55 @@ public class Program{
 	}
 
 }
+//-----------------------------------------------------------------------------------------------------------------------
+//Mesmo exemplo utilizando métodos no lugar de funções estáticas
+
+//Program.java
+package application;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+import util.Calculator;
+
+public class Program{
+	
+	public final double PI = 3.14159; //variável estática
+	
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Calculator calc = new Calculator();
+		
+		System.out.print("Enter radius: ");
+		double radius = sc.nextDouble();
+		
+		double c = calc.circunference(radius);
+		
+		double v = calc.volume(radius);
+		
+		System.out.printf("Circunference: %.2f\n", c);
+		System.out.printf("Volume: %.2f\n", v);
+		System.out.printf("PI value: %.2f\n", calc.PI);
+		
+		sc.close();		
+	}
+}
+
+//Calculator.java
+
+package util;
+
+public class Calculator {
+	public static double PI = 3.14159;
+	
+	public double circunference(double radius) {
+		return 2.0 * PI * radius;
+	}
+	public double volume(double radius) {
+		return 4.0 * PI * radius * radius * radius / 3.0;
+	}
+}
+
+
