dc29c130d999dd6e58c41f9fe6101d05badde163
Raylan Sales <Raylanwork@gmail.com>
2020-07-23 11:07:55 -0300

    Create data de agora.java

---
+++ códigos úteis/Java/data de agora.java
@@ -0,0 +1,12 @@
+package course;
+
+import java.util.Date;
+
+public class Program{
+	public static void main(String[] args) {
+		
+		//prints out the date and hour of now
+		System.out.println(new Date());
+		
+	}
+}
